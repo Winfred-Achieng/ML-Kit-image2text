ef7df4b9e32ea9bbcf2872f86619ee7af955cc5b
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-05-26 17:20:31 +0300

    camera permission

--- app/src/main/java/com/winfred/image2textapp/MainActivity2.java
+++ app/src/main/java/com/winfred/image2textapp/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.winfred.image2textapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button captureBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,17 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        captureBtn = findViewById(R.id.button);
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Intent intent = new Intent(MainActivity2.this, ScannerActivity.class);
+             startActivity(intent);
+
+            }
         });
 
 
--- app/src/main/java/com/winfred/image2textapp/ScannerActivity.java
+++ app/src/main/java/com/winfred/image2textapp/ScannerActivity.java
@@ -1,14 +1,34 @@
 package com.winfred.image2textapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ScannerActivity extends AppCompatActivity {
+
+    private ImageView captureIV;
+    private TextView resultTV;
+    private Button snapBtn;
+    private Button detectBtn;
+    private Bitmap imageBitmap;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +41,52 @@ public class ScannerActivity extends AppCompatActivity {
             return insets;
         });
 
+        captureIV = findViewById(R.id.IVCaptureImage);
+        resultTV = findViewById(R.id.TVDetectedText);
+        snapBtn = findViewById(R.id.buttonSnap);
+        detectBtn = findViewById(R.id.buttonDetect);
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectText();
+            }
+        });
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CheckPermission()){
+                    captureImage();
+                }else {
+                    requestPermission();
+                }
+            }
+        });
+
+    }
+
+    //permission method
+    private boolean CheckPermission(){
+        int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+        return  cameraPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission(){
+        int PERMISSION_CODE = 200;
+        ActivityCompat.requestPermissions(this,new String[]{
+                Manifest.permission.CAMERA
+        }, PERMISSION_CODE);
+    }
 
 
+
+    private void captureImage() {
+
+    }
+
+
+    private void detectText() {
+        // Your text detection logic here
     }
 }
